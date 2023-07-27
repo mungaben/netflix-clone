63b180c3a34b4535cf9e28e8a540042bbe6e5404
mungaben <kamaumungai3742@gmail.com>
2023-07-27 18:08:22 +0300

    Google login added

--- app/(Auth)/AuthUser/components/SocialLogin.tsx
+++ app/(Auth)/AuthUser/components/SocialLogin.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { signIn } from "next-auth/react";
 import React from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 import { FcGoogle } from "react-icons/fc";
@@ -7,11 +8,11 @@ import { FcGoogle } from "react-icons/fc";
 const SocialLogin = () => {
   return (
     <div className="flex flex-row items-center justify-center gap-4 ">
-      <div className="flex items-center justify-center w-10 h-10 transition duration-200 ease-in-out bg-white rounded-full cursor-pointer hover:opacity-80">
+      <div onClick={()=>signIn('google',{callbackUrl:'/'})} className="flex items-center justify-center w-10 h-10 transition duration-200 ease-in-out bg-white rounded-full cursor-pointer hover:opacity-80">
         <FcGoogle size={30} />
       </div>
 
-      <div className="flex items-center justify-center w-10 h-10 transition duration-200 ease-in-out bg-white rounded-full cursor-pointer hover:opacity-80">
+      <div onClick={()=>signIn('github',{callbackUrl:'/'})} className="flex items-center justify-center w-10 h-10 transition duration-200 ease-in-out bg-white rounded-full cursor-pointer hover:opacity-80">
         <FaGithub size={30} />
       </div>
     </div>
