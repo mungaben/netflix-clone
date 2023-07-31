0fb08b8e4067e98313c8195a1179ff62b7d2bd26
mungaben <kamaumungai3742@gmail.com>
2023-07-31 10:23:32 +0300

    final middleware

--- middleware.ts
+++ middleware.ts
@@ -36,9 +36,57 @@
 
 
 
-export { default } from "next-auth/middleware"
+import { getToken } from 'next-auth/jwt';
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+ 
+export async function middleware(request: NextRequest) {
+    // const session = await getServerSession({ req: request, ...authOptions })
 
 
+    // console.log('====================================');
+    // console.log("session available",session);
+    // console.log('====================================');
+    const secret = process.env.NEXTAUTH_JWT_SECRET;
+    if (secret === undefined) {
+        console.error("NEXT_AUTH_SECRET environment variable is not set.");
+        process.exit(1); // Optional: Exit the process if the secret is not set.
+      }
+
+    const token = await getToken({
+                req: request,
+                secret: secret,
+               
+            });
+
+            // const payload = jwt.verify(token);
+       
+        
+        
+        
+            //check not logged in
+            if (!token && request.nextUrl.pathname != "/AuthUser") {
+        
+                const url = new URL(`/AuthUser`, request.url);
+               
+                
+                return NextResponse.redirect(url.href);
+            }
 
 
-export const config = { matcher: [] }
+          
+
+//   if (request.nextUrl.pathname.startsWith('/about')) {
+//     return NextResponse.rewrite(new URL('/about-2', request.url))
+//   }
+ 
+//   if (request.nextUrl.pathname.startsWith('/dashboard')) {
+//     return NextResponse.rewrite(new URL('/dashboard/user', request.url))
+//   }
+}
+
+
+export const config = {
+    matcher: ['/Profiles/:path*', '/NavBar/:path*','/Api/:path*','/watch/:path*','/'],
+  }
