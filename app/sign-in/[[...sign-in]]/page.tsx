28a5fb65a6aa553c22ad9a9c1436fbfbc61d57ac
mungaben <kamaumungai3742@gmail.com>
2023-08-01 14:55:30 +0300

    Signin

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -8,7 +8,7 @@ export default function Page() {
   return (
     <div className="relative min-h-screen min-w-full  bg-[url('/hero.jpg')] bg-cover bg-no-repeat bg-center bg-fixed lg:opacity-80 bg-black">
       <div className="">
-        <SignIn afterSignInUrl={'/Profile'} />
+        <SignIn afterSignInUrl={'/Profiles'} />
       </div>
     </div>
   )
